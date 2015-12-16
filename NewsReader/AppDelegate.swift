ddd7fe9324ddff5c89db513a9e358e78a4579a4b
NSDengChen <714869773.qq.com>
2015-12-16 14:16:49 +0800

    first commit

---
+++ NewsReader/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  NewsReader
+//
+//  Created by dengchen on 15/12/16.
+//  Copyright © 2015年 name. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        self.window = UIWindow(frame: ScreenBounds)
+        
+        self.window?.rootViewController = DCMainPage()
+        
+        self.window?.makeKeyAndVisible()
+        
+        
+        return true
+    }
+
+
+}
+
---
+++ NewsReader/Global/ScreenSettingswift.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenSettingswift.swift
+//  NewsReader
+//
+//  Created by dengchen on 15/12/16.
+//  Copyright © 2015年 name. All rights reserved.
+//
+
+import UIKit
+
+let ScreenBounds = UIScreen.mainScreen().bounds
+let ScreenWidth  = UIScreen.mainScreen().bounds.width
+let ScreenHeight = UIScreen.mainScreen().bounds.height
